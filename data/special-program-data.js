c30c0553eee22a933106b165130b2962bdebca2b
GIDEOK <dupone@hanmail.net>
2026-05-12 22:21:11 +0900

    Update special program detail URL and add new test page
    - Changed the detail URL for the special program to point to the new test page.
    - Introduced `special-detail-01.html` as a new test page for the special exhibition, featuring a comprehensive layout and styling.

--- data/special-program-data.js
+++ data/special-program-data.js
@@ -3,7 +3,7 @@ window.SPECIAL_PROGRAM_DATA = [
     "id": 1,
     "title": "오다 카오리 지하 3부작 04/16(목)~4/21(화)",
     "thumbnail": "images/special/exhibition/special_exhibition_thumb_01.png",
-    "detailUrl": "special/exhibition/special-detail-01.html",
+    "detailUrl": "special/exhibition/test/special-detail-01.html",
     "sourceUrl": "http://55cine.com/2026/04/03/under03/"
   },
   {
